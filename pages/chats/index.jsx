878feacff5ca7dc87792c7972f41f0ec508b7ed2
Dhui-Park <hasong925@gmail.com>
2023-03-08 03:00:42 +0900

    Layout

--- pages/chats/index.jsx
+++ pages/chats/index.jsx
@@ -1,20 +1,24 @@
+import Layout from "@/components/layout";
+
 export default function Chats() {
   return (
-    <div className="divide-y-[1px] py-10">
-      {[1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex cursor-pointer items-center space-x-3 px-4 py-3"
-        >
-          <div className="h-12 w-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="font-semibold text-gray-700">Steve Jobs</p>
-            <p className="text-sm text-gray-500">
-              See you tomorrow in the corner at 2pm.
-            </p>
+    <Layout title="채팅" hasTabBar>
+      <div className="divide-y-[1px]">
+        {[1, 1, 1, 1, 1, 1].map((_, i) => (
+          <div
+            key={i}
+            className="flex cursor-pointer items-center space-x-3 px-4 py-3"
+          >
+            <div className="h-12 w-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="font-semibold text-gray-700">Steve Jobs</p>
+              <p className="text-sm text-gray-500">
+                See you tomorrow in the corner at 2pm.
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 }
--- pages/enter.jsx
+++ pages/enter.jsx
@@ -1,8 +1,5 @@
+import { cls } from "@/libs/utils";
 import { useState } from "react";
-
-function cls(...classnames) {
-  return classnames.join(" ");
-}
 
 export default function Enter() {
   const [method, setMethod] = useState("email");
