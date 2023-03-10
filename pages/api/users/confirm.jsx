912e79c28f07cf86fc14c45525dc505550a17810
Dhui-Park <hasong925@gmail.com>
2023-03-10 22:39:54 +0900

    iron-session part One

--- pages/api/users/confirm.jsx
+++ pages/api/users/confirm.jsx
@@ -1,10 +1,23 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import withHandler from "@/libs/server/withHandler";
 import client from "@/libs/server/client";
 
 async function handler(req, res) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).end();
+  req.session.user = {
+    id: exists.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "cocoharusession",
+  password: "987987987987987987987987aerwfgsfdfadfsdgaerfddsfargafgasdfsadfs",
+});
