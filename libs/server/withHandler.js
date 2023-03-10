6bca8e8d37313c92f150380072541609a97331a9
Dhui-Park <hasong925@gmail.com>
2023-03-11 04:52:58 +0900

    Protected Handlers

--- libs/server/withHandler.js
+++ libs/server/withHandler.js
@@ -1,10 +1,13 @@
-export default function withHandler(method, fn) {
+export default function withHandler({ method, handler, isPrivate = true }) {
   return async function (req, res) {
     if (req.method !== method) {
       return res.status(405).end();
     }
+    if (isPrivate && !req.session.user) {
+      return res.status(401).json({ ok: false, error: "Please log in." });
+    }
     try {
-      await fn(req, res);
+      await handler(req, res);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ error });
--- pages/api/users/enter.jsx
+++ pages/api/users/enter.jsx
@@ -41,4 +41,4 @@ async function handler(req, res) {
   });
 }
 
-export default withHandler("POST", handler);
+export default withHandler({ method: "POST", handler, isPrivate: false });
